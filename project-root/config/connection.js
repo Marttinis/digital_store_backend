bc4df92a69dd2dff432d10a3b27056e1e797457c
Gabriel Martins <gabrielmartins23445@gmail.com>
2024-11-25 14:19:54 -0300

    conexacao com o banco de dados usando sequelize

---
+++ project-root/config/connection.js
@@ -0,0 +1,12 @@
+const {Sequelize} = require("sequelize");
+
+
+//conexao com o banco de dados 
+const connection = new Sequelize({
+    dialect: 'mysql',
+    database: "blog",
+    host: "localhost",
+    username: "root",
+    password: "root",
+    port: "3306"
+})
