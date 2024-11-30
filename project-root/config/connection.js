03e1bffec7615ae0ccab4403a529e2edcef2b96f
Gabriel Martins <gabrielmartins23445@gmail.com>
2024-11-30 20:10:59 -0300

    adicionei dotenv na conexao com banco, e coloquei as informações no .env e emvModel, me preparando para fazer algumas mudanças no projeto

--- project-root/config/connection.js
+++ project-root/config/connection.js
@@ -1,15 +1,25 @@
+require("dotenv").config()
 const {Sequelize} = require("sequelize");
 
 
+
 //conexao com o banco de dados
-//.env precisa colocar aqui pra esconder a senha 
+
 const connection = new Sequelize({
-    dialect: 'mysql',
-    database: "blog",
-    host: "localhost",
-    username: "root",
-    password: "root",
-    port: "3306"
+    dialect: process.env.DB_DIALECT,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
 })
+
+connection.authenticate()
+   .then(() =>{
+      console.log('Conexão estabelecida com sucesso.');
+   })
+   .catch(()=>{
+      console.error('Não foi possivel se conectar ao banco de dados', err)
+   });
 
 module.exports = connection;
